9246fa251b91882db3482433ad94a32e69c523ce
Kristin <kristinponsonby@gmail.com>
2021-08-30 10:51:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code here
+function createEmployeeRecord(employee) {
+    timeInEvents = []
+    timeOutEvents = []
+    return employeeCard = {
+        firstName: employee[0]
+        familyName: employee[1]
+        title: employee[2]
+        payPerHour: employee[3]
+    }
+}
