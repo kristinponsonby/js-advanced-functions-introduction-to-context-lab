c452d1250c0322e699de4375fd0ee766d5a25984
Kristin <kristinponsonby@gmail.com>
2021-08-30 11:50:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function createEmployeeRecord(employee) {
-    timeInEvents = []
-    timeOutEvents = []
-    return employeeCard = {
-        firstName: employee[0]
-        familyName: employee[1]
-        title: employee[2]
-        payPerHour: employee[3]
+
+    const employeeCard = {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
     }
+    return employeeCard
 }
+
+function createEmployeeRecords(employeeRecords) {
+      const allEmployees = []
+       function createEmployeeRecord(employee) {
+           return employee
+       }
+
+    }
+
